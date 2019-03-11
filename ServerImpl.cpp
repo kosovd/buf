3c034f7b9c30b31ff653fc620987b821db3bf72a
DKosov <root@debian.kosov>
2019-03-11 09:01:41 -0400

    new

--- ServerImpl.cpp
+++ ServerImpl.cpp
@@ -1,1 +1,235 @@
+#include "ServerImpl.h"
 
+#include <cassert>
+#include <cstring>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <spdlog/logger.h>
+
+#include <afina/Storage.h>
+#include <afina/execute/Command.h>
+#include <afina/logging/Service.h>
+
+#include "protocol/Parser.h"
+
+namespace Afina {
+namespace Network {
+namespace MTblocking {
+
+// See Server.h
+ServerImpl::ServerImpl(std::shared_ptr<Afina::Storage> ps,
+                       std::shared_ptr<Logging::Service> pl)
+    : Server(ps, pl), _n_current(0) {}
+
+// See Server.h
+ServerImpl::~ServerImpl() {}
+
+// See Server.h
+void ServerImpl::Start(uint16_t port, uint32_t n_accept, uint32_t n_workers) {
+  _logger = pLogging->select("network");
+  _logger->info("Start mt_blocking network service");
+
+  sigset_t sig_mask;
+  sigemptyset(&sig_mask);
+  sigaddset(&sig_mask, SIGPIPE);
+  if (pthread_sigmask(SIG_BLOCK, &sig_mask, NULL) != 0) {
+    throw std::runtime_error("Unable to mask SIGPIPE");
+  }
+
+  struct sockaddr_in server_addr;
+  std::memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;         // IPv4
+  server_addr.sin_port = htons(port);       // TCP port number
+  server_addr.sin_addr.s_addr = INADDR_ANY; // Bind to any address
+
+  _server_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (_server_socket == -1) {
+    throw std::runtime_error("Failed to open socket");
+  }
+
+  int opts = 1;
+  if (setsockopt(_server_socket, SOL_SOCKET, SO_REUSEADDR, &opts,
+                 sizeof(opts)) == -1) {
+    close(_server_socket);
+    throw std::runtime_error("Socket setsockopt() failed");
+  }
+
+  if (bind(_server_socket, (struct sockaddr *)&server_addr,
+           sizeof(server_addr)) == -1) {
+    close(_server_socket);
+    throw std::runtime_error("Socket bind() failed");
+  }
+
+  if (listen(_server_socket, 5) == -1) {
+    close(_server_socket);
+    throw std::runtime_error("Socket listen() failed");
+  }
+
+  _n_accepted = n_accept;
+  running.store(true);
+  _thread = std::thread(&ServerImpl::OnRun, this);
+}
+
+// See Server.h
+void ServerImpl::Stop() {
+  running.store(false);
+  shutdown(_server_socket, SHUT_RDWR);
+}
+
+// See Server.h
+void ServerImpl::Join() {
+  std::unique_lock<std::mutex> lk(_m);
+  cv.wait(lk, [this]() { return _n_current.load() == 0; });
+  assert(_thread.joinable());
+  _thread.join();
+  close(_server_socket);
+}
+
+void ServerImpl::_runner(int client_socket) {
+  std::size_t arg_remains;
+  Protocol::Parser parser;
+  std::unique_ptr<Execute::Command> command_to_execute;
+  std::string argument_for_command;
+
+  try {
+    //		while(running.load()){
+    int readed_bytes = -1;
+    char client_buffer[4096];
+    while ((readed_bytes = read(client_socket, client_buffer,
+                                sizeof(client_buffer))) > 0) {
+      _logger->debug("Got {} bytes from socket", readed_bytes);
+      while (readed_bytes > 0) {
+        _logger->debug("Process {} bytes", readed_bytes);
+        if (!command_to_execute) {
+          std::size_t parsed = 0;
+          if (parser.Parse(client_buffer, readed_bytes, parsed)) {
+            _logger->debug("Found new command: {} in {} bytes", parser.Name(),
+                           parsed);
+            command_to_execute = parser.Build(arg_remains);
+            if (arg_remains > 0) {
+              arg_remains += 2;
+            }
+          }
+          if (parsed == 0) {
+            break;
+          } else {
+            std::memmove(client_buffer, client_buffer + parsed,
+                         readed_bytes - parsed);
+            readed_bytes -= parsed;
+          }
+        }
+        if (command_to_execute && arg_remains > 0) {
+          _logger->debug("Fill argument: {} bytes of {}", readed_bytes,
+                         arg_remains);
+          std::size_t to_read =
+              std::min(arg_remains, std::size_t(readed_bytes));
+          argument_for_command.append(client_buffer, to_read);
+          std::memmove(client_buffer, client_buffer + to_read,
+                       readed_bytes - to_read);
+          arg_remains -= to_read;
+          readed_bytes -= to_read;
+        }
+
+        if (command_to_execute && arg_remains == 0) {
+          _logger->debug("Start command execution");
+
+          std::string result;
+          command_to_execute->Execute(*pStorage, argument_for_command, result);
+
+          result += "\r\n";
+          if (send(client_socket, result.data(), result.size(), 0) <= 0) {
+            throw std::runtime_error("Failed to send response");
+          }
+          command_to_execute.reset();
+          argument_for_command.resize(0);
+          parser.Reset();
+        }
+      }
+    }
+    //	}
+  } catch (std::runtime_error &ex) {
+    _logger->error("SERVER_ERROR {}: {}", client_socket, ex.what());
+  }
+  close(client_socket);
+  _n_current--;
+  cv.notify_one();
+}
+
+// See Server.h
+void ServerImpl::OnRun() {
+  // Here is connection state
+  // - parser: parse state of the stream
+  // - command_to_execute: last command parsed out of stream
+  // - arg_remains: how many bytes to read from stream to get command argument
+  // - argument_for_command: buffer stores argument
+  std::size_t arg_remains;
+  Protocol::Parser parser;
+  std::string argument_for_command;
+  std::unique_ptr<Execute::Command> command_to_execute;
+  while (running.load()) {
+    _logger->debug("waiting for connection...");
+
+    // The call to accept() blocks until the incoming connection arrives
+    int client_socket;
+    struct sockaddr client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    if ((client_socket = accept(_server_socket, (struct sockaddr *)&client_addr,
+                                &client_addr_len)) == -1) {
+      continue;
+    }
+
+    // Got new connection
+    if (_logger->should_log(spdlog::level::debug)) {
+      std::string host = "unknown", port = "-1";
+
+      char hbuf[NI_MAXHOST], sbuf[NI_MAXSERV];
+      if (getnameinfo(&client_addr, client_addr_len, hbuf, sizeof(hbuf), sbuf,
+                      sizeof(sbuf), NI_NUMERICHOST | NI_NUMERICSERV) == 0) {
+        host = hbuf;
+        port = sbuf;
+      }
+      _logger->debug(
+          "Accepted connection on descriptor {} (host={}, port={})\n",
+          client_socket, host, port);
+    }
+
+    // Configure read timeout
+    {
+      struct timeval tv;
+      tv.tv_sec = 5; // TODO: make it configurable
+      tv.tv_usec = 0;
+      setsockopt(client_socket, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv,
+                 sizeof tv);
+    }
+
+    // TODO: Start new thread and process data from/to connection
+
+    {
+      if (_n_current < _n_accepted) {
+        _n_current++;
+        std::thread _t(&ServerImpl::_runner, this, client_socket);
+        _t.detach();
+      } else {
+        _logger->error("Sorry, max number of threads is: {}", _n_accepted);
+      }
+    }
+  }
+
+  // Cleanup on exit...
+  _logger->warn("Network stopped");
+}
+
+} // namespace MTblocking
+} // namespace Network
+} // namespace Afina
